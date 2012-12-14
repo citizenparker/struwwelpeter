a1fa17314cd8895fee2cf9221736a195ee4e18d1
sparker <scott.p.parker@gmail.com>
2012-12-13 18:14:26 -0600

    Preliminary support for metadata on story elements

--- src/struwwelpeter/stories.clj
+++ src/struwwelpeter/stories.clj
@@ -12,10 +12,10 @@
 (def story-templates
   [
    ;"a ?main-character who does not ?positive-action is consequently ?negative-condition."
-   "a ?naughty-descriptor ?main-character ?naughty-behavior. Eventually he is ?consequence-condition by ?consequence-actor, who ?consequence-action while ?main-character (actually he) is ?consequence-condition:2."
+   "a ?naughty-descriptor ?main-character ?naughty-behavior. Eventually ?main-character(he) is ?consequence-condition by ?consequence-actor, who ?consequence-action while ?main-character(he) is ?consequence-condition:2."
    ])
 
-(def main-character ["boy" "girl"])
+(def main-character [{:name "boy" :pronoun "he"} {:name "girl" :pronoun "she"}])
 (def positive-action ["brush teeth" "lap hands"])
 (def consequence-condition ["unpopular" "bedridden" "bitten"])
 (def consequence-action ["eats the ?main-character's sausage"])
@@ -27,12 +27,6 @@
   {:id (re-find #"^[\w-]*" s)
    :instance (re-find #"(?<=:)\w*" s)})
 
-(defn find-tokens [text]
-  (let [token-strs (re-seq #"(?<=\?)[\w:-]*" text)
-        tokens (map to-token token-strs)]
-    (zipmap (map token-key tokens)
-            tokens)))
-
 (defn token-coll [{:keys [id]}]
   @(resolve (symbol id)))
 
@@ -41,6 +35,12 @@
 
 (defn token-str [{:keys [id instance]}]
   (str "?" id (if instance (str ":" instance))))
+
+(defn find-tokens [text]
+  (let [token-strs (re-seq #"(?<=\?)[\w:-]*" text)
+        tokens (map to-token token-strs)]
+    (zipmap (map token-key tokens)
+            tokens)))
 
 (defn new-tokens-in-text [text context]
   (let [all-tokens (find-tokens text)
@@ -61,10 +61,16 @@
   (let [value (:value token)]
     (string/replace text (token-str token) value)))
 
+(defn random-item [coll]
+  (let [item (rand-nth coll)]
+    (if (map? item)
+      (:name item)
+      item)))
+
 (defn random-selection
   ([coll] (random-selection coll {}))
   ([coll token-context]
-    (let [text (rand-nth coll)
+    (let [text (random-item coll)
           new-tokens (new-tokens-in-text text token-context)
           new-context (reduce add-to-token-context token-context (vals new-tokens))]
       {:text (reduce substitute-from-tokens text (vals new-context))
